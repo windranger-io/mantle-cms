b4adb358c13a109a14f2f774bb4c306cc76e2d8d
octavioamu <octavioamuchastegui@gmail.com>
2023-06-13 15:16:50 -0300

    fix: set public url on railway

--- directus.config.js
+++ directus.config.js
@@ -12,7 +12,7 @@ module.exports = function (env) {
     // Reference: https://docs.railway.app/develop/variables#railway-provided-variables
     // PUBLIC_URL: `https://${env.RAILWAY_STATIC_URL}`,
     // PUBLIC_URL: `http://0.0.0.0:${env.PORT}`,
-    PUBLIC_URL: "/",
+    PUBLIC_URL: env.PUBLIC_URL,
 
     // Database variables from Railway PostgreSQL Plugin
     // Reference: https://docs.railway.app/plugins/postgresql
